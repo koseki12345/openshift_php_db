15a5bca770113639c25931dcc4fce8ff754b3c1e
ken-kawaguchi <ken-kawaguchi@test.com>
2016-03-10 20:40:19 +0900

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "<h1>Hello world</h1>";
-$mysqli = new mysqli(getenv("DATABASE_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
 if ($mysqli->connect_error) {
     exit($mysqli->connect_error);
 } else {
