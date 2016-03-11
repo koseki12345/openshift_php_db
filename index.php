6547f64fcbd08822c8ef9246356adc19230f1db9
ken-kawaguchi <ken-kawaguchi@test.com>
2016-03-11 09:43:06 +0900

    test1

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
 echo "<h1>Hello world</h1>";
-//$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
-echo getenv("MYSQL_USER") . ":" . getenv("MYSQL_PASSWORD") . ":" . getenv("MYSQL_DATABASE");
-//$mysqli = new mysqli("127.0.0.1", getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
-$mysqli = new mysqli("172.17.0.8", getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+echo getenv("HELLO_PHP_MYSQL_SERVICE_HOST") . ":" .getenv("MYSQL_USER") . ":" . getenv("MYSQL_PASSWORD") . ":" . getenv("MYSQL_DATABASE");
 if ($mysqli->connect_error) {
     exit($mysqli->connect_error);
 } else {
